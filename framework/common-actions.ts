b79daba639602f0db45770e90c94d70d362ad263
william melchior jablonski <willjabs2025@gmail.com>
2025-04-03 13:26:34 -0300

    refactory take screenshot

--- framework/common-actions.ts
+++ framework/common-actions.ts
@@ -1,6 +1,11 @@
 import type { Page } from 'playwright';
 
+//check isVisible function
 export async function isVisible(page: Page, locator: string): Promise<boolean> {
     await page.waitForSelector(locator);
     return await page.isVisible(locator);
+}
+//take screenshot function
+export async function takeScreenshot(page: Page, name: string) {
+    await page.screenshot({ path: `screenshots/${name}.png` });
 }
