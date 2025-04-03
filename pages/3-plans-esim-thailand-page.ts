6bdedf8896544d7658f446ee06cecdc6247ea22f
william <william@MBP-de-william.bbrouter>
2025-04-03 13:18:26 -0300

    refactory

--- pages/3-plans-esim-thailand-page.ts
+++ pages/3-plans-esim-thailand-page.ts
@@ -1,6 +1,5 @@
 import type { Page } from 'playwright';
 import { isVisible } from '../framework/common-actions';
-//import { mobilePlan } from './testdata';
 
 export class PlanPage {
     readonly page: Page;
@@ -13,21 +12,23 @@ export class PlanPage {
         return isVisible(this.page, 'div[xpath="/html/body/main/div[3]/div/astro-island/div/section/div/div[3]/div[1]/div[1]/p"]');
     }
     // check data of plan
-    async checkPlanValues() {
+    async checkPlanValues(country: String, data: string, valid: string, plan_type: string, price: string) {
+        
+            
         //check data of plan - Country: Thailand
-        const locator = page.locator('.title');
+        const locator = this.page.locator('.title');
         await expect(locator).toContainText('Thailand');
         //check data of plan -  Data: 5GB
-        const locator = page.locator('.title');
+        const locator = this.page.locator('.title');
         await expect(locator).toContainText('5GB');
         //check data of plan - Valid: 30 days
-        const locator = page.locator('.title');
+        const locator = this.page.locator('.title');
         await expect(locator).toContainText('30 days');
         //check data of plan - Plan type: data only
-        const locator = page.locator('.title');
+        const locator = this.page.locator('.title');
         await expect(locator).toContainText('data only');
         //check data of plan - Price 11,79 €
-        const locator = page.locator('.title');
+        const locator = this.page.locator('.title');
         await expect(locator).toContainText('11,79 €');
     }
 }
--- tests/test-price-telecom.spec.ts
+++ tests/test-price-telecom.spec.ts
@@ -6,30 +6,49 @@ import { LanguageCurrencyPage } from '../pages/2-language-currency-navigation-pa
 import { PlanPage } from '../pages/3-plans-esim-thailand-page';
 import { ScanQRCodePage } from '../pages/4-Scan-QR-code-page';
 
-test('When I\'m on page https://www.betterroaming.com/', async ({ page }) => {
+test('Given I\'m on page https://www.betterroaming.com/', async ({ page }) => {
     const homepage = new HomePage(page);
+    //----- feature: Check if the plan is correct
+    // Scenario: Check if the plan is correct
+    //
+    // Given I’m on page https://www.betterroaming.com/
+    // When I Select Euro as currency
+    // And Click on Thailand
+    // Then Check that the third plan has following values: Country: Thailand, Data: 5GB, Valid: 30 days, Plan type: data only, Price 11,79 €.
+    //-----
+    //#
+
     // When I'm on page https://www.betterroaming.com/
-    // And I Select Euro as currency
-    // And Click on Thailand
-    // Then Check that the third plan has following values: Country: Thailand, Data: 5GB, Valid: 30 days, Plan type: data only, Price 11,79 €.
     await homepage.open();
-    // check home page is opened
+    // I Check if home page is opened
+    await homepage.checkIfHomePageIsOpened();
     
-    await homepage.goToLoginPage();
-    //Select Euro as currency
-    await new LanguageCurrencyPage(page).login(user.email, user.password)
+  });
 
-    const userIsLoggedIn = await homepage.userIsLoggedIn();
-    expect(userIsLoggedIn).toBeTruthy();
-    // And Click on Thailand
-    await homepage.goToSettings();
-    await new PlanPage(page).logout();
-    // Then Check that the third plan has following values: Country: Thailand, Data: 5GB, Valid: 30 days, Plan type: data only, Price 11,79 €.
+  test('When I Select Euro as currency', async ({ page }) => {
+     //Select Euro as currency
+     await new LanguageCurrencyPage(page).selectCurrency(mobilePlan.currency)
+  });
+
+  test('And Click on Thailand', async ({ page }) => {
+        // And Click on Thailand
+        const homepage = new HomePage(page);
+        await homepage.fillAndSelectCountry(mobilePlan.country);
+  });
+
+  test('Then Check that the third plan has following values: Country: Thailand, Data: 5GB, Valid: 30 days, Plan type: data only, Price 11,79 €.', async ({ page }) => {
+     // Then Check that the third plan has following values: Country: Thailand, Data: 5GB, Valid: 30 days, Plan type: data only, Price 11,79 €.
+     await new PlanPage(page).checkPlanValues(mobilePlan.country, mobilePlan.data, mobilePlan.valid, mobilePlan.plan_type, mobilePlan.price);
     
+  });
+  
+  ////////////////////////////
+  test('When I select the plan', async ({ page }) => {
+   // When I select the plan 
+  });
 
-    // When I select the plan 
-
+  test('Then ScanQRCode page is opened', async ({ page }) => {
     // Then ScanQRCode page is opened
-    const userIsLoggedOut = await new ScanQRCodePage(page).userIsLoggedOut();
-    expect(userIsLoggedOut).toBeTruthy();
+    const scanQRCodePage = await new ScanQRCodePage(page).CheckTextScanQRcodetogetyourFREEeSIMnowExists();
+    expect(scanQRCodePage).toBeTruthy();
   });
