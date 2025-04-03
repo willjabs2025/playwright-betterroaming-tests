fd33d7cc7fa39ac49de14a5b7e48a79c7bb78c73
willjabs2025 <willjabs2025@gmail.com>
2025-04-03 11:57:49 -0300

    Update 3-plans-esim-thailand-page.ts

--- pages/3-plans-esim-thailand-page.ts
+++ pages/3-plans-esim-thailand-page.ts
@@ -7,8 +7,26 @@ export class LogoutPage {
     constructor(page: Page) {
         this.page = page;
     }
-
+    // check user is at correct page
     async userIsOnPlanPage(): Promise<boolean> {
         return isVisible(this.page, 'div[xpath="/html/body/main/div[3]/div/astro-island/div/section/div/div[3]/div[1]/div[1]/p"]');
+    }
+    
+    async checkPlanValues() {
+        //check data of plan - Country: Thailand
+        const locator = page.locator('.title');
+        await expect(locator).toContainText('Thailand');
+        //check data of plan -  Data: 5GB
+        const locator = page.locator('.title');
+        await expect(locator).toContainText('5GB');
+        //check data of plan - Valid: 30 days
+        const locator = page.locator('.title');
+        await expect(locator).toContainText('30 days');
+        //check data of plan - Plan type: data only
+        const locator = page.locator('.title');
+        await expect(locator).toContainText('data only');
+        //check data of plan - Price 11,79 €
+        const locator = page.locator('.title');
+        await expect(locator).toContainText('11,79 €');
     }
 }
